3403e16aac088f2ae302a643c83f588a087bece2
Vincent Melara <viincentmelara@gmail.com>
2024-03-12 02:59:07 -0700

    improved settings page, looks close to figma

--- cardiacengineering_app/lib/pages/dev_settings/settings_page.dart
+++ cardiacengineering_app/lib/pages/dev_settings/settings_page.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../login_auth/login_page.dart';
 
+import '../../components/background_gradient_container.dart';
+
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false, // Prevents bottom overflow
+      extendBodyBehindAppBar: true, // Extend the body behind the app bar
       appBar: AppBar(
-        title: Row(
+        backgroundColor: Colors.transparent, // Make app bar transparent
+        elevation: 0, // Remove app bar elevation
+        title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/logo.png',
-              height: 50.0,
-            ),
-            const SizedBox(
-                width: 5.0), // Add some space between the logo and text
-            const Text(
+            Text(
               'Settings',
               style: TextStyle(
                 fontSize: 30.0,
@@ -28,17 +28,51 @@ class SettingsPage extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(
+      body: BackgroundGradientContainer(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Image.asset(
+              width: double.infinity,
+              'assets/images/logo.png',
+              height: 100.0,
+            ),
+            const SizedBox(height: 100.0),
+            ElevatedButton(
+              onPressed: () {
+                //_logout(context);
+              },
+              child: const Text('Heart ID'),
+            ),
+            const SizedBox(height: 30.0),
+            ElevatedButton(
+              onPressed: () {
+                //_logout(context);
+              },
+              child: const Text('Developer Mode'),
+            ),
+            const SizedBox(height: 30.0),
             ElevatedButton(
               onPressed: () {
                 _logout(context);
               },
               child: const Text('Logout'),
             ),
-            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'App Version 1.2.0',
+              style: TextStyle(
+                fontSize: 15.0,
+              ),
+// Add some space between the logo and text
+            ),
           ],
         ),
       ),
