a1f4f035133ae64771dcc06f56ae1e6b8a90d288
timothyallec1 <112899554+timothyallec1@users.noreply.github.com>
2024-03-16 23:26:04 -0700

    new commit

--- cardiacengineering_app/python/CSVtoFirestore.py
+++ cardiacengineering_app/python/CSVtoFirestore.py
@@ -1,22 +1,23 @@
-import panda as pd
+import pandas as pd
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# initialize firebase admin sdk
-cred = credentials.Certificate('cardiacengineering_app\python\affinity-app-1018-firebase-adminsdk-uto31-3175c4cc5e.json')
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate('cardiacengineering_app\python\affinity-app-1018-firebase-adminsdk-uto31-3175c4cc5e.json')  # Replace with the correct path to your service account key file
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-#read csv file
-csv_file = "cardiacengineering_app\python\csv\data_1.csv"
+# Read CSV file
+csv_file = "cardiacengineering_app\python\csv\data_1.csv"  # Replace with the correct path to your CSV file
 df = pd.read_csv(csv_file)
 
-#convert data frame to dictionary
+# Convert DataFrame to dictionary
 data = df.to_dict(orient='records')
 
-#define firestore collection reference
-collection_ref = db.collection('')
+# Define Firestore collection reference
+collection_ref = db.collection('your_collection_name')  # Replace 'your_collection_name' with the name of your Firestore collection
 
+# Insert data into Firestore
 for item in data:
     collection_ref.add(item)
