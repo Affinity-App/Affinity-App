1b4315a745b5eac72becec7c708ee57ec09a7e2e
timothyallec1 <112899554+timothyallec1@users.noreply.github.com>
2024-03-27 20:06:44 -0700

    commit 3/27

--- cardiacengineering_app/lib/pages/home_data/home_page.dart
+++ cardiacengineering_app/lib/pages/home_data/home_page.dart
@@ -1,7 +1,7 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:jr_design_app/components/data_box.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import '../../components/background_gradient_container.dart';
 import '../../pages/dev_settings/settings_page.dart';
 import 'rpm_page.dart';
 import 'psi_page.dart';
@@ -9,118 +9,12 @@ import 'battery_page.dart';
 import 'gpm_page.dart';
 import 'record_now_page.dart';
 
+import '../../components/background_gradient_container.dart';
+
 typedef OnDataBoxPressedCallback = void Function(BuildContext context);
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String blood_pressure = '0'; // Initialize blood pressure with '0'
-  String bpm = '0'; // Initialize heart rate with '0'
-  String flow_rate = '0'; // Initialize flow rate with '0'
-  String power_consumption = '0'; // Initialize power consumption with '0'
-
-  @override
-  void initState() {
-    super.initState();
-    _initBloodPressureListener(); // Call function to listen for blood pressure changes
-    _initHeartRateListener(); // Call function to listen for heart rate changes'
-    _initFlowRateListener(); // Call function to listen for flow rate changes
-    _initPowerConsumptionListener(); // Call function to listen for power consumption changes
-  }
-
-  void _initBloodPressureListener() {
-    FirebaseFirestore.instance
-        .collection('sensor_data')
-        .doc('blood_pressure')
-        .snapshots()
-        .listen((DocumentSnapshot snapshot) {
-      if (snapshot.exists) {
-        String mmHg =
-            (snapshot.data() as Map<String, dynamic>)?['mmHg'] as String;
-        if (mmHg != null) {
-          setState(() {
-            blood_pressure = mmHg; // Update blood pressure state variable
-          });
-        } else {
-          setState(() {
-            blood_pressure = 'Unknown'; // Set to 'Unknown' if mmHg is null
-          });
-        }
-      }
-    });
-  }
-
-  void _initHeartRateListener() {
-    FirebaseFirestore.instance
-        .collection('sensor_data')
-        .doc('bpm')
-        .snapshots()
-        .listen((DocumentSnapshot snapshot) {
-      if (snapshot.exists) {
-        String beatsPerMin = (snapshot.data()
-            as Map<String, dynamic>)?['beats per minute'] as String;
-        if (beatsPerMin != null) {
-          setState(() {
-            bpm = beatsPerMin; // Update blood pressure state variable
-          });
-        } else {
-          setState(() {
-            bpm = 'Unknown'; // Set to 'Unknown' if mmHg is null
-          });
-        }
-      }
-    });
-  }
-
-  void _initFlowRateListener() {
-    FirebaseFirestore.instance
-        .collection('sensor_data')
-        .doc('flow_rate')
-        .snapshots()
-        .listen((DocumentSnapshot snapshot) {
-      if (snapshot.exists) {
-        String litersPerMinute = (snapshot.data()
-            as Map<String, dynamic>)?['liters per minute'] as String;
-        if (litersPerMinute != null) {
-          setState(() {
-            flow_rate = litersPerMinute; // Update blood pressure state variable
-          });
-        } else {
-          setState(() {
-            flow_rate = 'Unknown'; // Set to 'Unknown' if mmHg is null
-          });
-        }
-      }
-    });
-  }
-
-  void _initPowerConsumptionListener() {
-    FirebaseFirestore.instance
-        .collection('sensor_data')
-        .doc('power_consumption')
-        .snapshots()
-        .listen((DocumentSnapshot snapshot) {
-      if (snapshot.exists) {
-        String wattsPerHour = (snapshot.data()
-            as Map<String, dynamic>)?['watts per hour'] as String;
-        if (wattsPerHour != null) {
-          setState(() {
-            power_consumption =
-                wattsPerHour; // Update blood pressure state variable
-          });
-        } else {
-          setState(() {
-            power_consumption = 'Unknown'; // Set to 'Unknown' if mmHg is null
-          });
-        }
-      }
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -171,54 +65,34 @@ class _HomePageState extends State<HomePage> {
                   height: MediaQuery.of(context).padding.top +
                       kToolbarHeight +
                       20), // Added space
-              _buildDataBox(
-                context,
+              DataBox(
                 label: 'Blood Pressure',
-                value: '15.6',
+                value: '000',
                 iconPath: 'assets/images/Blood.png',
-                iconSize: 50.0,
-                labelFontSize: 20.0,
-                valueFontSize: 20.0,
-                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/PSIpage');
                 },
               ),
-              _buildDataBox(
-                context,
+              DataBox(
                 label: 'Heart Rate',
-                value: '60.2',
+                value: '000',
                 iconPath: 'assets/images/Heart.png',
-                iconSize: 50.0,
-                labelFontSize: 20.0,
-                valueFontSize: 20.0,
-                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/RPMpage');
                 },
               ),
-              _buildDataBox(
-                context,
+              DataBox(
                 label: 'Flow Rate',
-                value: '97%',
+                value: '00%',
                 iconPath: 'assets/images/Flow.png',
-                iconSize: 50.0,
-                labelFontSize: 20.0,
-                valueFontSize: 20.0,
-                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/GPMpage');
                 },
               ),
-              _buildDataBox(
-                context,
-                label: 'Battery Life',
-                value: '100',
+              DataBox(
+                label: 'Power Consumption',
+                value: '000',
                 iconPath: 'assets/images/Battery.png',
-                iconSize: 50.0,
-                labelFontSize: 20.0,
-                valueFontSize: 20.0,
-                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/BatteryPage');
                 },
