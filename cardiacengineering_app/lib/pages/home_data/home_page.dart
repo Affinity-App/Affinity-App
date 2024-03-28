bf283ab2708c0132f1ce547611222bc60cfb0091
timothyallec1 <112899554+timothyallec1@users.noreply.github.com>
2024-03-27 20:11:41 -0700

    merge firestore listener with testing for review

--- cardiacengineering_app/lib/pages/home_data/home_page.dart
+++ cardiacengineering_app/lib/pages/home_data/home_page.dart
@@ -1,20 +1,128 @@
-// ignore_for_file: unused_import
-
 import 'package:flutter/material.dart';
-import 'package:jr_design_app/components/data_box.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../components/background_gradient_container.dart';
 import '../../pages/dev_settings/settings_page.dart';
+import 'package:jr_design_app/components/data_box.dart';
 import 'rpm_page.dart';
 import 'psi_page.dart';
 import 'battery_page.dart';
 import 'gpm_page.dart';
 import 'record_now_page.dart';
-
 import '../../components/background_gradient_container.dart';
+import '../../pages/dev_settings/settings_page.dart';
 
 typedef OnDataBoxPressedCallback = void Function(BuildContext context);
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String blood_pressure = '0'; // Initialize blood pressure with '0'
+  String bpm = '0'; // Initialize heart rate with '0'
+  String flow_rate = '0'; // Initialize flow rate with '0'
+  String power_consumption = '0'; // Initialize power consumption with '0'
+
+  @override
+  void initState() {
+    super.initState();
+    _initBloodPressureListener(); // Call function to listen for blood pressure changes
+    _initHeartRateListener(); // Call function to listen for heart rate changes'
+    _initFlowRateListener(); // Call function to listen for flow rate changes
+    _initPowerConsumptionListener(); // Call function to listen for power consumption changes
+  }
+
+  void _initBloodPressureListener() {
+    FirebaseFirestore.instance
+        .collection('sensor_data')
+        .doc('blood_pressure')
+        .snapshots()
+        .listen((DocumentSnapshot snapshot) {
+      if (snapshot.exists) {
+        String mmHg =
+            (snapshot.data() as Map<String, dynamic>)?['mmHg'] as String;
+        if (mmHg != null) {
+          setState(() {
+            blood_pressure = mmHg; // Update blood pressure state variable
+          });
+        } else {
+          setState(() {
+            blood_pressure = 'Unknown'; // Set to 'Unknown' if mmHg is null
+          });
+        }
+      }
+    });
+  }
+
+  void _initHeartRateListener() {
+    FirebaseFirestore.instance
+        .collection('sensor_data')
+        .doc('bpm')
+        .snapshots()
+        .listen((DocumentSnapshot snapshot) {
+      if (snapshot.exists) {
+        String beatsPerMin = (snapshot.data()
+            as Map<String, dynamic>)?['beats per minute'] as String;
+        if (beatsPerMin != null) {
+          setState(() {
+            bpm = beatsPerMin; // Update blood pressure state variable
+          });
+        } else {
+          setState(() {
+            bpm = 'Unknown'; // Set to 'Unknown' if mmHg is null
+          });
+        }
+      }
+    });
+  }
+
+  void _initFlowRateListener() {
+    FirebaseFirestore.instance
+        .collection('sensor_data')
+        .doc('flow_rate')
+        .snapshots()
+        .listen((DocumentSnapshot snapshot) {
+      if (snapshot.exists) {
+        String litersPerMinute = (snapshot.data()
+            as Map<String, dynamic>)?['liters per minute'] as String;
+        if (litersPerMinute != null) {
+          setState(() {
+            flow_rate = litersPerMinute; // Update blood pressure state variable
+          });
+        } else {
+          setState(() {
+            flow_rate = 'Unknown'; // Set to 'Unknown' if mmHg is null
+          });
+        }
+      }
+    });
+  }
+
+  void _initPowerConsumptionListener() {
+    FirebaseFirestore.instance
+        .collection('sensor_data')
+        .doc('power_consumption')
+        .snapshots()
+        .listen((DocumentSnapshot snapshot) {
+      if (snapshot.exists) {
+        String wattsPerHour = (snapshot.data()
+            as Map<String, dynamic>)?['watts per hour'] as String;
+        if (wattsPerHour != null) {
+          setState(() {
+            power_consumption =
+                wattsPerHour; // Update blood pressure state variable
+          });
+        } else {
+          setState(() {
+            power_consumption = 'Unknown'; // Set to 'Unknown' if mmHg is null
+          });
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,45 +173,140 @@ class HomePage extends StatelessWidget {
                   height: MediaQuery.of(context).padding.top +
                       kToolbarHeight +
                       20), // Added space
-              DataBox(
-                label: 'Blood Pressure',
-                value: '000',
+              _buildDataBox(
+                context,
+                label: "Blood Pressure",
+                value: blood_pressure + ' mmHg',
                 iconPath: 'assets/images/Blood.png',
+                iconSize: 50.0,
+                labelFontSize: 20.0,
+                valueFontSize: 20.0,
+                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/PSIpage');
                 },
               ),
-              DataBox(
+              _buildDataBox(
+                context,
                 label: 'Heart Rate',
-                value: '000',
+                value: bpm + ' BPM',
                 iconPath: 'assets/images/Heart.png',
+                iconSize: 50.0,
+                labelFontSize: 20.0,
+                valueFontSize: 20.0,
+                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/RPMpage');
                 },
               ),
-              DataBox(
+              _buildDataBox(
+                context,
                 label: 'Flow Rate',
-                value: '00%',
+                value: flow_rate + ' L/min',
                 iconPath: 'assets/images/Flow.png',
+                iconSize: 50.0,
+                labelFontSize: 20.0,
+                valueFontSize: 20.0,
+                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/GPMpage');
                 },
               ),
-              DataBox(
+              _buildDataBox(
+                context,
                 label: 'Power Consumption',
-                value: '000',
+                value: power_consumption + ' watts/hr',
                 iconPath: 'assets/images/Battery.png',
+                iconSize: 50.0,
+                labelFontSize: 20.0,
+                valueFontSize: 20.0,
+                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/BatteryPage');
                 },
               ),
-              DataBox(
+              _buildDataBox(
+                context,
                 label: 'Record Now',
                 value: '',
                 iconPath: 'assets/images/logo.png',
+                iconSize: 50.0,
+                labelFontSize: 20.0,
+                valueFontSize: 0,
+                borderRadius: 20.0,
                 onPressed: (context) {
                   Navigator.pushNamed(context, '/RecordNow');
                 },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDataBox(
+    BuildContext context, {
+    required String label,
+    required String value,
+    required String iconPath,
+    required double iconSize,
+    required double labelFontSize,
+    required double valueFontSize,
+    required double borderRadius,
+    required OnDataBoxPressedCallback onPressed,
+  }) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 1.2,
+      child: GestureDetector(
+        onTap: () {
+          onPressed(context);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(borderRadius),
+            border: Border.all(color: Colors.black, width: 1.0),
+          ),
+          margin: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center, // Adjusted to center
+            children: [
+              Image.asset(
+                iconPath,
+                height: iconSize,
+                width: iconSize,
+              ),
+              const SizedBox(width: 10.0),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center, // Adjusted to center
+                  children: [
+                    Text(
+                      label,
+                      style: TextStyle(
+                        fontSize: labelFontSize,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center, // Adjusted to center
+                    ),
+                    const SizedBox(height: 5.0),
+                    Text(
+                      value,
+                      style: TextStyle(
+                        fontSize: valueFontSize,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 86, 140, 234),
+                      ),
+                      textAlign: TextAlign.center, // Adjusted to center
+                    ),
+                  ],
+                ),
+              ),
+              Icon(
+                Icons.add,
+                color: Colors.black,
               ),
             ],
           ),
