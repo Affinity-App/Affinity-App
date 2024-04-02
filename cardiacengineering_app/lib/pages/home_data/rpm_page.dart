79fa900bc13b48811f8f318bf90970d02f9ec815
timothyallec1 <112899554+timothyallec1@users.noreply.github.com>
2024-04-02 13:43:27 -0700

    successful pull of rpm firestore data into table for 1 session

--- cardiacengineering_app/lib/pages/home_data/rpm_page.dart
+++ cardiacengineering_app/lib/pages/home_data/rpm_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:jr_design_app/components/background_gradient_container.dart';
 import 'package:jr_design_app/pages/dev_settings/test_chart.dart';
 
 class RPMpage extends StatelessWidget {
-  const RPMpage({super.key});
+  const RPMpage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +21,66 @@ class RPMpage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-
+            const SizedBox(height: 50.0), // Added space below the title
             Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(18.0),
                 color: Colors.blueGrey,
               ),
-              child: const LineChartSample2(),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: const LineChartSample2(), // Added padding to the chart
+              ),
             ),
-            const SizedBox(height: 30.0),
+            Expanded(
+              child: StreamBuilder<DocumentSnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('large_heart_data')
+                    .doc('bpm')
+                    .collection('session 03-28-24 12:04')
+                    .doc('data')
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<DocumentSnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  }
+                  final Map<String, dynamic> data =
+                      snapshot.data!.data() as Map<String, dynamic>;
+                  final List<dynamic> dataArray = data['data'] as List<dynamic>;
+
+                  // Prepare lists for Y and X values
+                  List<String> yValues = [];
+                  List<String> xValues = [];
+                  dataArray.forEach((map) {
+                    yValues.add(map['y_value'] as String);
+                    xValues.add(map['x_value'] as String);
+                  });
+
+                  return SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      columns: [
+                        DataColumn(label: Text('Time(s)')),
+                        DataColumn(label: Text('Value (mmHg)')),
+                      ],
+                      rows: List<DataRow>.generate(
+                        yValues.length,
+                        (int index) => DataRow(
+                          cells: [
+                            DataCell(Text(xValues[index])),
+                            DataCell(Text(yValues[index])),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
             ElevatedButton(
               onPressed: () {
                 // Navigate back to the previous page (Home page)
@@ -36,7 +88,6 @@ class RPMpage extends StatelessWidget {
               },
               child: const Text('Back to Home'),
             ),
-            const SizedBox(height: 30.0),
           ],
         ),
       ),
