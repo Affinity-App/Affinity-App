aab4a739ac47cb582e1e35eabe0b1059d530e714
timothyallec1 <112899554+timothyallec1@users.noreply.github.com>
2024-04-02 15:29:16 -0700

    made drop down menu transparent backround

--- cardiacengineering_app/lib/pages/home_data/rpm_page.dart
+++ cardiacengineering_app/lib/pages/home_data/rpm_page.dart
@@ -30,7 +30,7 @@ class _RPMpageState extends State<RPMpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false, // Prevents bottom overflow
-      extendBodyBehindAppBar: true, //
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent, // Make app bar transparent
         elevation: 0, // Remove app bar elevation
@@ -42,76 +42,113 @@ class _RPMpageState extends State<RPMpage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const SizedBox(height: 50.0), // Added space below the title
-            DropdownButton<int>(
-              value: selectedSessionIndex,
-              onChanged: (int? newIndex) {
-                if (newIndex != null) {
-                  changeSession(newIndex);
-                }
-              },
-              items: List.generate(sessionNames.length, (index) {
-                return DropdownMenuItem<int>(
-                  value: index,
-                  child: Text(sessionNames[index]),
-                );
-              }),
-            ),
             Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18.0),
-                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(color: Colors.black),
+                color: Colors
+                    .transparent, // Set the container background to transparent
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: const LineChartSample2(), // Added padding to the chart
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: DropdownButton<int>(
+                value: selectedSessionIndex,
+                onChanged: (int? newIndex) {
+                  if (newIndex != null) {
+                    changeSession(newIndex);
+                  }
+                },
+                dropdownColor: Colors
+                    .transparent, // Set dropdown box background to transparent
+                items: List.generate(sessionNames.length, (index) {
+                  return DropdownMenuItem<int>(
+                    value: index,
+                    child: Text(sessionNames[index]),
+                  );
+                }),
+              ),
+            ),
+            const SizedBox(height: 16.0), // Added spacing below the dropdown
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18.0),
+                  color: Colors.blueGrey,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: const LineChartSample2(), // Added padding to the chart
+                ),
               ),
             ),
             Expanded(
-              child: StreamBuilder<DocumentSnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('large_heart_data')
-                    .doc('bpm')
-                    .collection(sessionNames[selectedSessionIndex])
-                    .doc('data')
-                    .snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<DocumentSnapshot> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  }
-                  final Map<String, dynamic> data =
-                      snapshot.data!.data() as Map<String, dynamic>;
-                  final List<dynamic> dataArray = data['data'] as List<dynamic>;
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('large_heart_data')
+                      .doc('bpm')
+                      .collection(sessionNames[selectedSessionIndex])
+                      .doc('data')
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    }
+                    final Map<String, dynamic> data =
+                        snapshot.data!.data() as Map<String, dynamic>;
+                    final List<dynamic> dataArray =
+                        data['data'] as List<dynamic>;
 
-                  // Prepare lists for Y and X values
-                  List<String> yValues = [];
-                  List<String> xValues = [];
-                  dataArray.forEach((map) {
-                    yValues.add(map['y_value'] as String);
-                    xValues.add(map['x_value'] as String);
-                  });
+                    // Prepare lists for Y and X values
+                    List<String> yValues = [];
+                    List<String> xValues = [];
+                    dataArray.forEach((map) {
+                      yValues.add(map['y_value'] as String);
+                      xValues.add(map['x_value'] as String);
+                    });
 
-                  return SingleChildScrollView(
-                    child: DataTable(
-                      columns: [
-                        DataColumn(label: Text('Time(s)')),
-                        DataColumn(label: Text('Value (mmHg)')),
-                      ],
-                      rows: List<DataRow>.generate(
-                        yValues.length,
-                        (int index) => DataRow(
-                          cells: [
-                            DataCell(Text(xValues[index])),
-                            DataCell(Text(yValues[index])),
+                    return SingleChildScrollView(
+                      child: Container(
+                        padding: EdgeInsets.all(
+                            8.0), // Adjust the padding to include the border width
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.black,
+                              width: 2.0), // Adjust the border thickness
+                          borderRadius:
+                              BorderRadius.circular(10.0), // Set border radius
+                        ),
+                        child: DataTable(
+                          headingRowColor: MaterialStateColor.resolveWith(
+                              (states) => Colors
+                                  .white), // Set the background color of the header row
+                          headingTextStyle: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight
+                                  .bold), // Set the text style of the header row
+                          columns: [
+                            DataColumn(label: Text('Time(s)')),
+                            DataColumn(label: Text('Value (mmHg)')),
                           ],
+                          rows: List<DataRow>.generate(
+                            yValues.length,
+                            (int index) => DataRow(
+                              cells: [
+                                DataCell(Text(xValues[index])),
+                                DataCell(Text(yValues[index])),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
             ElevatedButton(
