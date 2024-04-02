3e1c24170452a236dcb269dcce519af9c9ff45b7
Brian Lee <kwanghak.lee@calbaptist.edu>
2024-04-01 23:42:35 -0700

    Charts and buttons edited
    Chart color changed and border added.
    Animation and color added to all the buttons.

--- cardiacengineering_app/lib/pages/home_data/psi_page.dart
+++ cardiacengineering_app/lib/pages/home_data/psi_page.dart
@@ -33,7 +33,11 @@ class PSIpage extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(18.0),
-                color: Color.fromRGBO(247, 169, 186, 1.0),
+                border: Border.all(
+                  width: 2.0,
+                  color: Colors.black,
+                ),
+                color: Colors.white,
               ),
               child: const LineChartSample2(),
             ),
@@ -43,6 +47,33 @@ class PSIpage extends StatelessWidget {
                 // Navigate back to the previous page (Home page)
                 Navigator.pop(context);
               },
+              style: ButtonStyle(
+                  // backgroundColor
+                  backgroundColor: MaterialStateProperty.all<Color>(const Color.fromRGBO(247, 169, 186, 1.0)), // set background color to pink
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.black), // Set text color to white
+                // Add the animation controller
+                animationDuration: const Duration(milliseconds: 200),
+                // Shrink on press
+                overlayColor: MaterialStateProperty.resolveWith<Color>(
+                  (states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.white10; // Shrink and visually indicate press
+                    }
+                    return Colors.transparent; // Use default overlay color
+                  },
+                ),
+                // Scale the button down slightly on press
+                padding: MaterialStateProperty.resolveWith<EdgeInsets>(
+                  (states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 8.0);
+                    }
+                    return const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 12.0);
+                  },
+                ),
+              ),
               child: const Text('Back to Home'),
             ),
             const SizedBox(height: 30.0),
