6acc4d3d6a870821ba7f42922927c63426475605
timothyallec1 <112899554+timothyallec1@users.noreply.github.com>
2024-04-04 08:00:13 -0700

    added table for rpm page

--- cardiacengineering_app/lib/pages/home_data/psi_page.dart
+++ cardiacengineering_app/lib/pages/home_data/psi_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:jr_design_app/components/background_gradient_container.dart';
 import 'package:jr_design_app/pages/dev_settings/test_chart.dart';
 import 'package:jr_design_app/pages/home_data/battery_page.dart';
 import 'package:jr_design_app/pages/home_data/gpm_page.dart';
 import 'package:jr_design_app/pages/home_data/home_page.dart';
+import 'package:jr_design_app/pages/home_data/psi_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:jr_design_app/pages/home_data/rpm_page.dart';
-import '../../components/background_gradient_container.dart';
 
 class PSIpage extends StatefulWidget {
   const PSIpage({Key? key}) : super(key: key);
@@ -14,8 +16,20 @@ class PSIpage extends StatefulWidget {
 }
 
 class _PSIpageState extends State<PSIpage> {
-  // Initially selected option
-  String _selectedOption = 'Blood Pressure';
+  late int selectedSessionIndex = 0;
+  final List<String> sessionNames = [
+    "session 04-04-24 07:06",
+    "session 04-04-24 07:10",
+    "session 04-04-24 07:23",
+    "session 04-04-24 07:24",
+    "session 04-04-24 07:25"
+  ];
+
+  void changeSession(int index) {
+    setState(() {
+      selectedSessionIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,39 +40,38 @@ class _PSIpageState extends State<PSIpage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: DropdownButton<String>(
-          value: _selectedOption,
+          value: 'Blood Pressure', // Default value is 'RPM Data'
           icon: Text('\u25BC',
               style: TextStyle(color: Colors.grey[800], fontSize: 25.0)),
           underline: Container(height: 0),
           onChanged: (String? newValue) {
             setState(() {
-              _selectedOption = newValue!;
+              // Navigate based on the selected option
+              switch (newValue) {
+                case 'RPM Data':
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RPMpage()),
+                  );
+                  break;
+                case 'Flow Rate GPM':
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GPMpage()),
+                  );
+                  break;
+                case 'Power Consumption':
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Batterypage()),
+                  );
+                  break;
+                // Add more cases for other options as needed
+                // Default case for 'Blood Pressure' is to do nothing
+                default:
+                  break;
+              }
             });
-            // Navigate based on the selected option
-            switch (newValue) {
-              case 'RPM Data':
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RPMpage()),
-                );
-                break;
-              case 'Flow Rate GPM':
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GPMpage()),
-                );
-                break;
-              case 'Power Consumption':
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Batterypage()),
-                );
-                break;
-              // Add more cases for other options as needed
-              // Default case for 'Blood Pressure' is to do nothing
-              default:
-                break;
-            }
           },
           items: <String>[
             'Blood Pressure',
@@ -90,19 +103,106 @@ class _PSIpageState extends State<PSIpage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            const SizedBox(height: 100.0), // Added space below the title
+            Container(
+              decoration: BoxDecoration(
+                color: Colors
+                    .transparent, // Set the container background to transparent
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: DropdownButton<int>(
+                value: selectedSessionIndex,
+                onChanged: (int? newIndex) {
+                  if (newIndex != null) {
+                    changeSession(newIndex);
+                  }
+                },
+                dropdownColor:
+                    Colors.white, // Set dropdown box background to transparent
+                items: List.generate(sessionNames.length, (index) {
+                  return DropdownMenuItem<int>(
+                    value: index,
+                    child: Text(
+                      sessionNames[index],
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold), // Make text bold
+                    ),
+                  );
+                }),
+              ),
+            ),
+            const SizedBox(height: 16.0), // Added spacing below the dropdown
             Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(18.0),
-                border: Border.all(
-                  width: 2.0,
-                  color: Colors.black,
-                ),
+                border: Border.all(width: 2.0, color: Colors.black),
                 color: Colors.white,
               ),
               child: const LineChartSample2(),
             ),
-            const SizedBox(height: 30.0),
-            // Removed the Back to Home button
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: StreamBuilder<DocumentSnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('large_heart_data')
+                      .doc('blood pressure')
+                      .collection(sessionNames[selectedSessionIndex])
+                      .doc('data')
+                      .snapshots(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    }
+                    final Map<String, dynamic> data =
+                        snapshot.data!.data() as Map<String, dynamic>;
+                    final List<dynamic> dataArray =
+                        data['data'] as List<dynamic>;
+
+                    // Prepare lists for Y and X values
+                    List<String> yValues = [];
+                    List<String> xValues = [];
+                    dataArray.forEach((map) {
+                      yValues.add(map['y_value'] as String);
+                      xValues.add(map['x-value'] as String);
+                    });
+
+                    return SingleChildScrollView(
+                      child: Container(
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 2.0),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: DataTable(
+                          headingRowColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.white),
+                          headingTextStyle: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                          columns: [
+                            DataColumn(label: Text('Time(s)')),
+                            DataColumn(label: Text('Value (mmHg)')),
+                          ],
+                          rows: List<DataRow>.generate(
+                            yValues.length,
+                            (int index) => DataRow(
+                              cells: [
+                                DataCell(Text(xValues[index])),
+                                DataCell(Text(yValues[index])),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
