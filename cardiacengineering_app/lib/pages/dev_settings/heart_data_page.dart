8a5f405cf9fb7045ce9dab31019b09c1f78a6845
Brian Lee <kwanghak.lee@calbaptist.edu>
2024-04-04 00:44:54 -0700

    Animation added for back button
    -Heart ID back butoon animation added

--- cardiacengineering_app/lib/pages/dev_settings/heart_data_page.dart
+++ cardiacengineering_app/lib/pages/dev_settings/heart_data_page.dart
@@ -43,6 +43,37 @@ class HeartDataPage extends StatelessWidget {
                 // Navigate back to the previous page
                 Navigator.pop(context);
               },
+              style: ButtonStyle(
+                // backgroundColor
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    const Color.fromRGBO(
+                        247, 169, 186, 1.0)), // set background color to pink
+                foregroundColor: MaterialStateProperty.all<Color>(
+                    Colors.black), // Set text color to white
+                // Add the animation controller
+                animationDuration: const Duration(milliseconds: 200),
+                // Shrink on press
+                overlayColor: MaterialStateProperty.resolveWith<Color>(
+                  (states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors
+                          .white10; // Shrink and visually indicate press
+                    }
+                    return Colors.transparent; // Use default overlay color
+                  },
+                ),
+                // Scale the button down slightly on press
+                padding: MaterialStateProperty.resolveWith<EdgeInsets>(
+                  (states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 8.0);
+                    }
+                    return const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 12.0);
+                  },
+                ),
+              ),
               child: const Text('Back'),
             ),
             const SizedBox(height: 30.0),
