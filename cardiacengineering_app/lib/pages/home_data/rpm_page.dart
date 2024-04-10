00b2cf70419dbb9efd9b4d9b9dc45dee51e4f46f
Vincent Melara <viincentmelara@gmail.com>
2024-04-10 16:26:55 -0700

    removed unnecessary button from RPM page

--- cardiacengineering_app/lib/pages/home_data/rpm_page.dart
+++ cardiacengineering_app/lib/pages/home_data/rpm_page.dart
@@ -218,44 +218,6 @@ class _RPMpageState extends State<RPMpage> {
                 ),
               ),
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to the previous page (Home page)
-                Navigator.pop(context);
-              },
-                 style: ButtonStyle(
-                // backgroundColor
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color.fromRGBO(
-                        247, 169, 186, 1.0)), // set background color to pink
-                foregroundColor: MaterialStateProperty.all<Color>(
-                    Colors.black), // Set text color to white
-                // Add the animation controller
-                animationDuration: const Duration(milliseconds: 200),
-                // Shrink on press
-                overlayColor: MaterialStateProperty.resolveWith<Color>(
-                  (states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return Colors
-                          .white10; // Shrink and visually indicate press
-                    }
-                    return Colors.transparent; // Use default overlay color
-                  },
-                ),
-                // Scale the button down slightly on press
-                padding: MaterialStateProperty.resolveWith<EdgeInsets>(
-                  (states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return const EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 8.0);
-                    }
-                    return const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 12.0);
-                  },
-                ),
-              ),
-              child: const Text('Back to Home'),
-            ),
           ],
         ),
       ),
